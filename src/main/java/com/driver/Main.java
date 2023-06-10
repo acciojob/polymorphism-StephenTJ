2f16cd102fe427159782c51d0c137ca39998d963
“Stephen <stephen.jose@creysto.com>
2023-06-10 18:06:53 +0530

    added all the overloaded functions

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,24 @@ package com.driver;
 
 public class Main {
 
+    static class Product{
+        public int product(int x, int y) {
+            return 0;
+        }
+        public int product(int x, int y, int z) {
+            return 1;
+        }
+        public double product(double x, double y) {
+            return 0.0;
+        }
+
+    }
+    public static void main(String args[]){
+        Main.Product p=new Main.Product();
+        p.product(0,0);
+        p.product(0,0,0);
+        p.product(0.0,0.0);
+
+    }
+
 }
